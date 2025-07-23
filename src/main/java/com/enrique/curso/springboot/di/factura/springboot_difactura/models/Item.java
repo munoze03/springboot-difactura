a883d701a17612bd7d105cfa1ca7a03a25157a7b
Enrique <munoze03@gmail.com>
2025-07-24 00:17:36 +0200

    Factura y relaciones entre sus componentes

--- src/main/java/com/enrique/curso/springboot/di/factura/springboot_difactura/models/Item.java
+++ src/main/java/com/enrique/curso/springboot/di/factura/springboot_difactura/models/Item.java
@@ -2,4 +2,20 @@ package com.enrique.curso.springboot.di.factura.springboot_difactura.models;
 
 public class Item {
 
+    private Product product;
+    private Integer quantity;
+
+    public Product getProduct() {
+        return product;
+    }
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+    public Integer getQuantity() {
+        return quantity;
+    }
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
 }
--- src/main/java/com/enrique/curso/springboot/di/factura/springboot_difactura/models/Product.java
+++ src/main/java/com/enrique/curso/springboot/di/factura/springboot_difactura/models/Product.java
@@ -2,4 +2,19 @@ package com.enrique.curso.springboot.di.factura.springboot_difactura.models;
 
 public class Product {
 
+    private String name;
+    private Integer price;
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public Integer getPrice() {
+        return price;
+    }
+    public void setPrice(Integer price) {
+        this.price = price;
+    }
 }
