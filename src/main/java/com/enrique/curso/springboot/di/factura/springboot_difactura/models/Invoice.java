cb7d7a26113635ca09e3c3243c2743801186576e
Enrique <munoze03@gmail.com>
2025-07-24 00:53:59 +0200

    Calculando total de la Factura

--- src/main/java/com/enrique/curso/springboot/di/factura/springboot_difactura/models/Invoice.java
+++ src/main/java/com/enrique/curso/springboot/di/factura/springboot_difactura/models/Invoice.java
@@ -38,6 +38,16 @@ public class Invoice {
     }
 
     public int getTotal(){
-        return 0;
+        // int total = 0;
+
+        // for (Item item : items) {
+        //     total+=item.getImporte();
+        // }
+
+        int total = items.stream()
+        .map(item -> item.getImporte())
+        .reduce(0, (sum, importe) -> sum + importe);
+
+        return total;
     }
 }
